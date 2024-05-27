9da68cf40df9d710e8fa43a01700cdcd22cdc4d3
Han Sol Jin <hansol@hansol.ca>
2024-05-27 15:47:19 -0700

    Add a page generator
    Combined with gen_mirror_json output, this lets us generate a device
    page based on a predefined YAML.
    
    The generator will use this YAML to append the download links for
    the frontend.

--- _device/mh2lm.md
+++ _device/mh2lm.md
@@ -1,12 +1,13 @@
 ---
 title: G8X ThinQ / V50S ThinQ
 manufacturer: LG
-description:
 categories:
 - lge
+images:
+- url: /assets/images/devices/mh2lm.png
+kernel:
+- url: https://github.com/lge-qcom-dev/android_kernel_lge_sm8150
 downloads:
   lineage-20.0-20240228-UNOFFICIAL-mh2lm.zip:
-    url: https://sourceforge.net/projects/lifehackerhansol-android/files/lineage/mh2lm/lineage-20.0-20240228-UNOFFICIAL-mh2lm.zip
-images: 
-- url: /assets/images/devices/mh2lm.png
+    url: https://sourceforge.net/projects/lifehackerhansol/android/files/lineage/mh2lm/lineage-20.0-20240228-UNOFFICIAL-mh2lm.zip
 ---
---
+++ gen_pages.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import yaml
+
+
+def gen_page(device: str):
+    with open(f"_data/devices/{device}.yml", 'r') as deviceYaml:
+        deviceData = yaml.safe_load(deviceYaml)
+
+    with open(f"ota/{device}", 'r') as buildJson:
+        buildData = json.load(buildJson)
+
+    # Initialize downloads section
+    deviceData['downloads'] = {}
+
+    for buildDevice in buildData:
+        for build in buildData[buildDevice]:
+            # Initialize download item
+            deviceData['downloads'][build['filename']] = {}
+            deviceData['downloads'][build['filename']]['url'] = build['filepath']
+
+    # Reverse sort this so that the latest link is the first one
+    deviceData['downloads'] = dict(sorted(deviceData['downloads'].items(), reverse=True))
+
+    # Rewrite page front matter
+    # That's the only thing on this file anyway
+    with open(f'_device/{device}.md', 'w') as devicePage:
+        devicePage.write('---\n')
+        yaml.dump(deviceData, devicePage, sort_keys=False)
+        devicePage.write('---')
+
+
+if __name__ == "__main__":
+    for i in os.listdir('_data/devices/'):
+        device = i.replace('.yml', '')
+        print(f"Generating {device} page...")
+        gen_page(device)
